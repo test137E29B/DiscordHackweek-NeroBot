4eb2968e8f6efa605aa99b15bd12e1f55775986a
Nameless <badxnameless@gmail.com>
2019-06-25 19:37:43 +0100

    I am the Computer Man I can do anything you can.

--- src/commands/Moderation/ban.ts
+++ src/commands/Moderation/ban.ts
@@ -1,4 +1,5 @@
 import { Command, CommandStore, KlasaMessage, KlasaClient } from "klasa";
+import { GuildMember } from "discord.js";
 
 export default class extends Command {
   public constructor(
@@ -17,10 +18,14 @@ export default class extends Command {
     });
   }
 
-  public async run(msg: KlasaMessage, args): Promise<any> {
+  public async run(
+    msg: KlasaMessage,
+    args: [GuildMember, String]
+  ): Promise<any> {
     const { flags } = msg;
     const [user, reason] = args;
-
+    if (user.user.id === this.client.user.id)
+      return msg.send(this.client.languages.get("en-US").get("COMPUTER_MAN"));
     if (!user.bannable) return msg.sendLocale("COMMAND_BAN_NOT", [user]);
 
     const days: number = "7d" in flags ? 7 : "1d" in flags ? 1 : 0;

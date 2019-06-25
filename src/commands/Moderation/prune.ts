c8166d75bd67900c2bd540fe58fd08c9b5953db0
jpsl00 <18708801+jpsl00@users.noreply.github.com>
2019-06-25 16:22:49 -0300

    Prune

---
+++ src/commands/Moderation/prune.ts
@@ -0,0 +1,45 @@
+import { Command, CommandStore, KlasaMessage, KlasaClient } from "klasa";
+
+export default class extends Command {
+  public constructor(
+    client: KlasaClient,
+    store: CommandStore,
+    file: string[],
+    dir: string
+  ) {
+    super(client, store, file, dir, {
+      requiredPermissions: ["MANAGE_MESSAGES", "EMBED_LINKS"],
+      permissionLevel: 5,
+      description: lang => lang.get("COMMAND_PRUNE_DESCRIPTION"),
+      extendedHelp: lang => lang.get("COMMAND_PRUNE_EXTENDED"),
+      runIn: ["text"],
+      aliases: ["purge", "clean"],
+      usage: "<Amount:number{1,50}> (Channel:channel)"
+    });
+
+    this.customizeResponse("Amount", msg =>
+      msg.language.get("COMMAND_PRUNE_INVALID")
+    );
+  }
+
+  public async run(msg: KlasaMessage, args): Promise<any> {
+    const [amount, channel] = args;
+
+    let target;
+
+    if (!channel) target = msg.channel;
+    else target = channel;
+
+    return target
+      .bulkDelete(
+        await target.messages.fetch({
+          limit: amount,
+          before: msg.id
+        })
+      )
+      .then(messages =>
+        msg.sendLocale("COMMAND_PRUNE_DONE", [messages, amount, target])
+      )
+      .catch(() => msg.sendLocale("ERR"));
+  }
+}
